0645a8cabb84bde785633ab3a833c9a89740de34
liujing <liuj-ai@glodon.com>
2024-05-10 10:46:54 +0800

    测试资源隔离

--- fe/fe-core/src/main/java/com/starrocks/qe/SetExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SetExecutor.java
@@ -106,7 +106,10 @@ public class SetExecutor {
                 throw new DdlException("only allow set resource isolation for other user," +
                         " current user: " + currentUser.getUser());
             }
-            LOG.info("=================>> Set Resource Isolation Var Successful! <<=================");
+            LOG.info("=================>> Set Resource Isolation Var Successful!, " +
+                            "user: {}, hosts: {}  <<=================",
+                    setResourceIsolationVar.getUserIdent().getUser(),
+                    setResourceIsolationVar.getHosts());
         }
     }
 
