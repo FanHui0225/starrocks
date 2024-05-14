1af874f06288a01fddd8ef8886c386bb6e913317
liujing <liuj-ai@glodon.com>
2024-05-14 11:16:48 +0800

    Revert "测试资源隔离"
    This reverts commit 7b0e2fde

--- fe/fe-core/src/main/java/com/starrocks/qe/scheduler/Deployer.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/scheduler/Deployer.java
@@ -187,9 +187,6 @@ public class Deployer {
             }
 
             for (FragmentInstance instance : stageInstances) {
-                LOG.info("Debug -> resource isolation " +
-                        "Fragment instance worker: {}.", instance.getWorker());
-
                 TExecPlanFragmentParams request =
                         tFragmentInstanceFactory.create(instance, curDescTable, accTabletSinkDop, totalTableSinkDop);
                 if (enablePipelineTableSinkDop) {
