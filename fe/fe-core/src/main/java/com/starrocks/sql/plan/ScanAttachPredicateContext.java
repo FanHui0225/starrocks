d31118843b854ccfc72e9e704797f55698b116dc
liujing <liuj-ai@glodon.com>
2024-06-29 20:04:43 +0800

    测试谓词下推

--- fe/fe-core/src/main/java/com/starrocks/sql/plan/ScanAttachPredicateContext.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/plan/ScanAttachPredicateContext.java
@@ -38,9 +38,9 @@ import org.apache.logging.log4j.Logger;
 
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -55,7 +55,7 @@ public final class ScanAttachPredicateContext {
     private static final ThreadLocal<ScanAttachPredicateContext>
             SCAN_ATTACH_PREDICATE_CONTEXT = new ThreadLocal<>();
 
-    private static final Map<Type, Integer> NUMERIC_TYPE_RANKS = new TreeMap<>();
+    private static final Map<Type, Integer> NUMERIC_TYPE_RANKS = new HashMap<>();
 
     static {
         int order = 0;
