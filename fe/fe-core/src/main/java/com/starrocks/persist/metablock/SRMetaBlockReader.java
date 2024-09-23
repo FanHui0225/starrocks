645e688c509a3ed75cb00ab7257cb9bbebe0cb42
liujing <liuj-ai@glodon.com>
2024-09-23 18:42:17 +0800

    还原测试

--- fe/fe-core/src/main/java/com/starrocks/persist/metablock/SRMetaBlockReader.java
+++ fe/fe-core/src/main/java/com/starrocks/persist/metablock/SRMetaBlockReader.java
@@ -122,9 +122,9 @@ public class SRMetaBlockReader {
         String s = Text.readStringWithChecksum(checkedInputStream);
         SRMetaBlockFooter footer = GsonUtils.GSON.fromJson(s, SRMetaBlockFooter.class);
         if (checksum != footer.getChecksum()) {
-            LOG.error(String.format(
-                    "Invalid meta block, checksum mismatch! expect %d actual %d",
-                    footer.getChecksum(), checksum));
+            throw new SRMetaBlockException(String.format(
+                    "Invalid meta block, checksum mismatch! expect %d actual %d", footer.getChecksum(), checksum));
+
         }
     }
 }
